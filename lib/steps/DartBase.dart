7d9cb6ff58b7bb92de391c1769b780c70e9fcb66
Rostislav Maslov <maslov.rostislav@gmail.com>
2022-06-11 16:47:56 +0300

    Урок #2 - Типы данных

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,5 +1,28 @@
 class DartBase{
   void run(){
     print('DartBase: hello world!');
+
+    _stepConf();
+    _stepDataType();
+  }
+
+  void _stepConf(){
+    print('_stepConf: This is step 1');
+  }
+
+  void _stepDataType(){
+    String str = 'st';
+    int intVar = 1;
+    double doubleVar = 1.1;
+
+    print('_stepDataType: $str');
+    print('_stepDataType: $intVar');
+    print('_stepDataType: $doubleVar');
+
+    var sum = intVar + doubleVar;
+    print('_stepDataType: $sum');
+
+    DartBase db = DartBase();
+    db._stepConf();
   }
 }
